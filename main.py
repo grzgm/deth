a35d59b4d4f800bd8c4a85b45cf098b04ebb4303
Grzegorz <g.malisz@student.fontys.nl>
2023-12-09 15:34:36 +0100

    qlearning done from scratch

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import numpy as np
 
 
 class MDP:
-    def __init__(self, states, actions, transition_probabilities, rewards, start_state, gamma=0.9,
+    def __init__(self, states, actions, transition_probabilities, rewards, gamma=0.9,
                  eps=1e6, random_termination=0.0, cost_of_living=0.0):
         self.states = states
         self.actions = actions
         self.transition_probabilities = transition_probabilities
         self.inspect_probabilities()
         self.rewards = rewards
-        self.current_state = start_state
 
         self.gamma = gamma
         self.eps = eps
@@ -42,14 +41,14 @@ class MDP:
             for action in state.values():
                 assert isclose(sum(action.values()), 1, abs_tol=1e-4)
 
-    def step(self, action):
-        next_states = list(transition_probabilities[self.current_state][action].keys())
-        probabilities = list(transition_probabilities[self.current_state][action].values())
+    def step(self, current_state, action):
+        next_states = list(transition_probabilities[current_state][action].keys())
+        probabilities = list(transition_probabilities[current_state][action].values())
         new_state = np.random.choice(next_states, p=probabilities)
-        reward = self.lookup_reward(self.current_state, action, new_state)
+        reward = self.lookup_reward(current_state, action, new_state)
         is_terminal = len(transition_probabilities[new_state]) == 0
-        self.current_state = new_state
-        return (self.current_state, reward, is_terminal)
+        current_state = new_state
+        return (current_state, reward, is_terminal)
 
     # def value(self, state: str):
     #     pass
@@ -88,50 +87,80 @@ class MDP:
             for state in self.states:
                 self.value[state] = self.action_value(state, self.policy(state))
 
-if __name__ == "__main__":
-    states = ['0', '1', '2', '3', '4']
-    actions = ['l', 'r']
 
-    # Terminal States have no actions
-    transition_probabilities = {
-        '0': {},
-        '1': {'l': {'0': 1},
-              'r': {'2': 1}},
-        '2': {'l': {'1': 1},
-              'r': {'3': 1}},
-        '3': {'l': {'2': 1},
-              'r': {'4': 1}},
-        '4': {},
-    }
+# if __name__ == "__main__":
+states = ['0', '1', '2', '3', '4', '5', '6']
+actions = ['d', 'u']
 
-    rewards = {
-        '1': {'l': {'0': -1}},
-        '3': {'r': {'4': 1}},
-    }
+# Terminal States have no actions
+transition_probabilities = {
+    '0': {},
+    '1': {'d': {'0': 1},
+          'u': {'2': 1}},
+    '2': {'d': {'1': 1},
+          'u': {'3': 1}},
+    '3': {'d': {'2': 1},
+          'u': {'4': 1}},
+    '4': {'d': {'3': 1},
+          'u': {'5': 1}},
+    '5': {'d': {'4': 1},
+          'u': {'6': 1}},
+    '6': {},
+}
 
-    state_value_array = []
-    action_value_array = np.zeros((len(states), len(actions)))
+rewards = {
+    '1': {'d': {'0': 1}},
+    '5': {'u': {'6': 0}},
+}
 
-    episodes = 1
-    max_steps_in_episode = 100
-    epsilon = 1.00
-    start_state = '1'
+state_value_array = []
+action_value_array = np.zeros((len(states), len(actions)))
 
-    mdp = MDP(states, actions, transition_probabilities, rewards, start_state, random_termination=0.3, cost_of_living=-1.5)
+episodes = 1000
+max_steps_in_episode = 1000
+start_state = states[2]
+
+# Learning Rate
+alpha = 0.04
+# Exploration Rate
+epsilon = 1
+# Discount Factor
+gamma = 0.9
+
+mdp = MDP(states, actions, transition_probabilities, rewards, random_termination=0.3, cost_of_living=-1.5)
 
 for episode in range(episodes):
-    new_state = start_state
+    previous_state = start_state
     total_reward = 0
 
     for step in range(max_steps_in_episode):
         # Choose action based on epsilon-greedy policy
         if np.random.rand() < epsilon:
-            action = np.random.choice(mdp.possible_actions(mdp.current_state))
+            # random action
+            action = np.random.choice(mdp.possible_actions(previous_state))
         else:
-            action = actions[np.argmax(action_value_array[states.index(new_state), :])]
+            # best action
+            possible_actions_indexes = [index for index, element in enumerate(actions) if element in mdp.possible_actions(previous_state)]
+            # Extract the elements at the allowed indexes
+            allowed_elements = action_value_array[states.index(previous_state), possible_actions_indexes]
 
-        new_state, reward, is_terminal = mdp.step(action)
+            # Find the index of the element with the highest value
+            max_index_in_allowed = np.unravel_index(allowed_elements.argmax(), allowed_elements.shape)
+
+            # Convert the index from allowed_elements back to the original my_array
+            max_index_in_original = (states.index(previous_state), possible_actions_indexes[max_index_in_allowed[0]])
+            action = actions[max_index_in_original[1]]
+
+        new_state, reward, is_terminal = mdp.step(previous_state, action)
+
+        action_value_array[states.index(previous_state), actions.index(action)] = (1-alpha) * action_value_array[states.index(previous_state), actions.index(action)] + alpha*(reward + gamma * max(action_value_array[states.index(new_state), :]))
+
+        previous_state = new_state
         print(mdp.possible_actions(new_state, ))
-        print(new_state, reward, is_terminal)
+        # print(new_state, reward, is_terminal)
+        print(new_state, reward)
         if (is_terminal):
             break
+
+with np.printoptions(precision=3, suppress=True):
+    print(action_value_array)
